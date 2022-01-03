5bb46cdc8a629fd743844e180d0d240139ec6aca
Jason Tang <jasontang14@hotmail.com>
2022-01-03 14:45:37 +0800

    Updated by Jason03/01

--- Routers/ListingRouter.js
+++ Routers/ListingRouter.js
@@ -1,25 +1,26 @@
 class ListingRouter {
-    constructor(express, listingservice) {
-      this.express = express;
-      this.listingservice = listingservice;
-    }
-  
-    router() {
-      let router = this.express.Router();
-  
-      router.post("/", this.post.bind(this));
-  
-      return router;
-    }
-  
-    post(req, res) {
-      console.log("Posting", req.body);
-      return this.listingservice
-       .list(req.body.starttime, req.body.endtime)
-    .then((data) => res.json(data))
-    .catch((err) => res.status(500).json(err));
-    }
-  
+  constructor(express, listingservice) {
+    this.express = express;
+    this.listingservice = listingservice;
   }
-  
-  module.exports = ListingRouter;
+
+  router() {
+    let router = this.express.Router();
+
+    router.post("/", this.post.bind(this));
+
+    return router;
+  }
+
+  post(req, res) {
+    console.log(
+      `Posting StartTime:${req.body.starttime},  EndTime:${req.body.endtime} AT ${req.body.location}`
+    );
+    return this.listingservice
+      .list(req.body.starttime, req.body.endtime, req.body.location)
+      .then((data) => res.render("listing", data))
+      .catch((err) => res.status(500).json(err));
+  }
+}
+
+module.exports = ListingRouter;
